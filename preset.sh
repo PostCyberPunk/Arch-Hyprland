21755d7a89ef5fb18e92e3abda2cf83feb67bb08
PostCyberPunk <lyhguest2@outlook.com>
2024-01-04 20:30:00 +0800

    add preset file

---
+++ preset.sh
@@ -0,0 +1,13 @@
+# !!!!!Make sure you use the right answer or it may failed
+###### yay or paru 
+aur_helper="yay"
+#Y or N
+nvidia="Y"
+gtk_themes="Y"
+bluetooth="Y"
+thunar="Y"
+sddm="Y"
+xdph="Y"
+zsh="N"
+rog="N"
+dots="N"
