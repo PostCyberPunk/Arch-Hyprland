611536b49f3f6eff77a29ebe97a1146e2653a913
PostCyberPunk <lyhguest2@outlook.com>
2024-01-23 09:45:12 +0800

    additional pkg

---
+++ install-scripts/pkgs.sh
@@ -0,0 +1,30 @@
+basic=(
+	firefox
+	peazip
+	spotify
+	translate-shell
+	obs-studio
+	vlc
+)
+dev=(
+	rust
+	python
+	dotnet-sdk
+	dotnet-runtime
+	visual-studio-code-bin
+	unityhub
+)
+pretui=(
+	neofetch
+	neo-matrix
+	pipes.sh
+	peaclock
+	nyancat
+)
+gamimng=(
+	xone-dkms-git
+	xone-dongle-firmware
+	antimicrox
+	steam
+	itch-bin
+)
